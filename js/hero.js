5f0081f65a6a98b68da01ea812b98b1c2e32be75
dagobert renouf <dagobert.king@gmail.com>
2014-01-16 19:39:08 +0100

    Draft: tried to make both responsive width AND responsive height work

--- js/hero.js
+++ js/hero.js
@@ -1,5 +1,5 @@
 $(document).ready( function(){
-	
+
 	heroResize();
 
 });
@@ -9,8 +9,29 @@ $( window ).resize(function() {
 });
 
 function heroResize(){	
-	if($(window).height() > 500 && $(window).height() < 993){
-		var heroHeight =  $(window).height() / 5.2360679775 * 4.2360679775;
-		$(".hero-unit img").css("height", heroHeight);
-	}
+	
+	// resizing the hero height to make sure people see an inviting amount of white space bellow it
+	var wh = $(window).height(); //window's height
+	var hh = $(".hero-unit").height(); // hero's height
+
+	var ws = (wh - hh); // white space available
+	var wsReq = hh / 6.85410196625; // white space required
+	var hhReq = wh - wsReq; // hero unit height we need to make width fit
+	// console.log(ws + " vs " + wsReq);
+
+	if(ws < wsReq){
+		console.log(hhReq);
+		$(".hero-unit").css("height", hhReq);
+	} 
+	// if(spaceToHeroRatio > 6.85410196625)
+	// 	console.log("trop petit!");
+	// else
+		// console.log("bien");
+	// console.log($(window).height() / $(window).width());
+	// if($(window).height() > 197 && $(window).height() < 920){
+	// 	var heroHeight =  $(window).height() / 7.85410196625 * 6.85410196625;
+	// 	$(".hero-unit").css("height", heroHeight);
+	// }
 }
+
+// 6.85410196625 / 1.61803398875
