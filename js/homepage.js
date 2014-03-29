f8a57c40476184a6e68db5e37d399ec1f2ade7c2
dagobert renouf <dagobert.king@gmail.com>
2014-03-29 16:11:08 +0100

    Added no javascript fallback support for homepage image

--- js/homepage.js
+++ js/homepage.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
+$('#scene').prepend('<li class="layer" data-depth="1.00"><img src="/images/homepage/lossy/parallax/depth-1.png" alt="portfolio webdesigner image 1"></li><li class="layer" data-depth="0.75"><img src="/images/homepage/lossy/parallax/depth-0-75.png" alt="portfolio webdesigner image 2"></li><li class="layer" data-depth="0.50"><img src="/images/homepage/lossy/parallax/depth-0-5.png" alt="portfolio webdesigner image 3"></li><li class="layer" data-depth="0.25"><img src="/images/homepage/lossy/parallax/depth-0-25.png" alt="portfolio webdesigner image 4"></li><li class="layer" data-depth="0.05"><img src="/images/homepage/lossy/parallax/depth-0.png" alt="portfolio webdesigner image 5"></li>');
 
+$(document).ready(function(){
     portfolioPanelParallax();
     testimoniesPanelCarousel();
     $('.scroll-for-more').smoothScroll();
-
-});
+})
 
 function portfolioPanelParallax(){
   $('#scene').parallax({
